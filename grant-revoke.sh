6cbc3797fa6e08a476c82389da38201637c776aa
ariesrulez2 <subhash.april2@gmail.com>
2019-04-08 14:53:21 +0000

    grant and revoke

---
+++ grant-revoke.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#==============================================================
+#
+# Author : ariesrulez2
+# Purpose : grant-revoke-user-on multiinstance
+# Date : 8/04/2019
+#
+#======================================================================
+
+User1="DevOps"
+sudo useradd $User1
+resultuser=`awk -F: '{ print $1}' /etc/passwd | grep "$User1"`
+echo $resultuser
+if [ "$resultuser" == "$User1" ]; then
+echo "user created successfully"
+fi
+SSH_PUBLIC_KEY=`sudo su "$User1" -c "ssh-keygen -b 4096 -t rsa -f /tmp/id_rsa1 -C '$User1@xyz.com' -q  -N '' ; cat /tmp/id_rsa1.pub" `
+# add keys to grant user access
+#usermod -a -G wheel $User1
+
+# add the ssh public key
+ssh 10.1.1.32 -c "echo $SSH_PUBLIC_KEY >> .ssh/authorised_keys"
+
