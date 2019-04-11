42e3a455a05e7202fcef684e6e454e1a67ee4a38
ariesrulez2 <subhash.april2@gmail.com>
2019-04-11 11:04:00 +0000

    added and revoke user from multiple instance

--- grant-revoke.sh
+++ grant-revoke.sh
@@ -3,21 +3,51 @@
 #
 # Author : ariesrulez2
 # Purpose : grant-revoke-user-on multiinstance
-# Date : 8/04/2019
+# Date : 11/04/2019
 #
 #======================================================================
 
-User1="DevOps"
-sudo useradd $User1
+User1="user3"
+UserAddSshAcess(){
+#Add  user
+sudo adduser --force-badname  $User1
+#Add user to Dev Group
+sudo adduser $User1 Dev
+#Check if user added
 resultuser=`awk -F: '{ print $1}' /etc/passwd | grep "$User1"`
 echo $resultuser
 if [ "$resultuser" == "$User1" ]; then
 echo "user created successfully"
 fi
-SSH_PUBLIC_KEY=`sudo su "$User1" -c "ssh-keygen -b 4096 -t rsa -f /tmp/id_rsa1 -C '$User1@xyz.com' -q  -N '' ; cat /tmp/id_rsa1.pub" `
-# add keys to grant user access
-#usermod -a -G wheel $User1
+#enerate public SShkey for user
+SSH_PUBLIC_KEY=`sudo su $User1 -c "ssh-keygen -b 4096 -t rsa -f /home/$User1/.ssh/id_rsa -C '$User1@xyz.com' -q  -N '' ; cat /home/$User1/.ssh/id_rsa " `
 
-# add the ssh public key
-ssh 0.0.0.0 -c "echo $SSH_PUBLIC_KEY >> .ssh/authorised_keys"
+sudo cat  /home/"$User1"/.ssh/id_rsa > authorized_keys 
+#Copy key to admin users authorized key
+scp authorized_keys adminuser@$hostname:/home/adminuser
+ssh adminuser@$hostname "sudo adduser -m -p 12345 $User1; mkdir /home/$User1/.ssh/ "   
+#Move public key to users authorized key file
+ssh adminuser@$hostname "sudo mv /home/adminuser/authorized_keys /home/$User1/.ssh/authorized_keys"
+#Change owner
+ssh adminuser@$hostname "sudo chown $User1:$User1 /home/$User1/.ssh/authorized_keys"
 
+
+}
+
+UserRevokeSshAcess(){
+ssh adminuser@$hostname "sudo cd /home/$User1/; rm ~/.ssh/authorized_keys"
+
+}
+#Main Call
+
+#Get VM ips  From txt file
+NoOfVms=`cat hostnames.txt| sort | uniq | wc -l`
+ echo "Total number of VMS: " $NoOfVms
+ for (( i=1; i<=$NoOfVms; i++ ))
+   do
+    CurrVM=`cat hostnames.txt | head -$i | tail -1`
+    echo $CurrVM
+     UserAddSshAcess
+     UserRevokeSshAcess
+ done
+   
